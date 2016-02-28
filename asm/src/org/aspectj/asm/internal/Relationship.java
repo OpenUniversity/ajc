bf507894a4651c05a561a9f686694055160bf262
Arik Hadas <arik.hadas@openu.ac.il>
2016-02-28 22:35:08 +0200

    store optional custom source line as part of a relationship

--- asm/src/org/aspectj/asm/internal/Relationship.java
+++ asm/src/org/aspectj/asm/internal/Relationship.java
@@ -30,6 +30,7 @@ public class Relationship implements IRelationship {
 	private String sourceHandle;
 	private List<String> targets;
 	private boolean hasRuntimeTest;
+        private Integer line;
 
 	public Relationship(String name, Kind kind, String sourceHandle, List<String> targets, boolean runtimeTest) {
 		this.name = name;
@@ -83,4 +84,11 @@ public class Relationship implements IRelationship {
 		return isAffects;
 	}
 
+	public Integer getSourceLine() {
+		return line;
+	}
+
+	public void setSourceLine(int line) {
+		this.line = line;
+	}
 }
