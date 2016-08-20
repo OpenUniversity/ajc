6daefd0b9594291129a5f9838c3e2efce552d478
Arik Hadas <arik.hadas@openu.ac.il>
2016-08-20 20:38:42 +0300

    change the retention policy of the hide annotation to the default (class)

--- aspectj5rt/java5-src/org/aspectj/lang/annotation/HideMethod.java
+++ aspectj5rt/java5-src/org/aspectj/lang/annotation/HideMethod.java
@@ -2,10 +2,7 @@ package org.aspectj.lang.annotation;
 
 import java.lang.annotation.Target;
 import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
 
-@Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 public @interface HideMethod {
     MethodJoinpointType[] joinpoints() default { MethodJoinpointType.EXECUTION, MethodJoinpointType.CALL, MethodJoinpointType.ALL_WITHIN };
--- aspectj5rt/java5-src/org/aspectj/lang/annotation/HideStaticInitialization.java
+++ aspectj5rt/java5-src/org/aspectj/lang/annotation/HideStaticInitialization.java
@@ -2,10 +2,7 @@ package org.aspectj.lang.annotation;
 
 import java.lang.annotation.Target;
 import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
 
-@Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 public @interface HideStaticInitialization {
     boolean hideJoinpointsWithin() default true;
